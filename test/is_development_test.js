c23fb65c2d3b9a22b8fa61de8c2bb7c5b882bac8
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-31 09:28:53 +0900

    Initial commit

---
+++ test/is_development_test.js
@@ -0,0 +1,28 @@
+/**
+ * Test case for isDevelopment.
+ * Runs with mocha.
+ */
+'use strict'
+
+const isDevelopment = require('../lib/is_development.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('is-development', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Is development', () => co(function * () {
+    process.env.NODE_ENV = 'development'
+    assert.ok(isDevelopment())
+  }))
+})
+
+/* global describe, before, after, it */
